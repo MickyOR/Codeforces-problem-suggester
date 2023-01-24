e88e36c5688a0264f21fd7234a0010ed69f68172
MickyOr <mickyor97@gmail.com>
2023-01-24 17:40:18 -0400

    problemas ignorados y contestId con index

--- problemSuggester.py
+++ problemSuggester.py
@@ -6,6 +6,11 @@ from userProblems import getGroupSolvedProblems
 
 url = 'https://codeforces.com/api/'
 
+ignoredProblemNames = [
+  'Подкрутка II',
+  'Подкрутка I'
+]
+
 # Handles should be a list of strings
 # Tags should be a list of strings separated by semicolons
 # Ratings should be a list of integers
@@ -22,16 +27,21 @@ def suggestProblems(handles, tags, ratings, count = 10):
   unsolvedProblems = []
   problems = getProblems(url, method, options, ratings)
   for problem in problems:
+    if 'name' not in problem or 'contestId' not in problem or 'index' not in problem: 
+      continue
+    if problem['name'] in ignoredProblemNames:
+      continue
     if problem['name'] not in solvedProblems:
-      unsolvedProblems.append(problem['name'])
+      unsolvedProblems.append([str(problem['contestId'])+problem['index'], problem['name']])
+      print(problem)
     if len(unsolvedProblems) == count:
       break
   return unsolvedProblems
   
 
-handles = ['MickyOr', 'jnava1612', 'Anghelo']
-tags = ['combinatorics']
-ratings = [1100, 1200, 1300]
+handles = ['MickyOr', 'jnava1612', 'Anghelo', 'OPF10']
+tags = ['']
+ratings = [1100, 1200, 1300, 1400, 1500]
 count = 3
 problems = suggestProblems(handles, tags, ratings, count)
 for problem in problems:
